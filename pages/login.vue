0e10a9b1400801da171817079c4b6fb12d94b811
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-08-31 21:11:47 +0300

    update: mb fixed issue with logging cuz on vercel could not log in

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="main">
-    <h1>Wait logging you in...</h1>
+    <transition name="fade" mode="out-in">
+      <button v-if="!loggingIn" @click="login">Log in</button>
+      <h1 v-else>Wait, Logging you in...</h1>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'login',
-  mounted() {
-    return this.login()
-  },
+  data: () => ({
+    loggingIn: false,
+  }),
   methods: {
     login() {
+      this.loggingIn = false
       this.$auth.loginWith('auth0')
     },
   },
@@ -23,5 +27,30 @@ export default {
   min-height: 100vh;
   display: grid;
   place-items: center;
+}
+
+button {
+  appearance: none;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  border: 1px solid #ccc;
+  background-color: transparent;
+
+  font: inherit;
+  font-size: 1.1rem;
+  text-transform: uppercase;
+
+  cursor: pointer;
+  transition: transform 0.4s ease;
+
+  &:focus,
+  &:hover {
+    transition: none;
+    transform: scale(1.01);
+  }
+  &:active {
+    transition: none;
+    transform: scale(0.99);
+  }
 }
 </style>
