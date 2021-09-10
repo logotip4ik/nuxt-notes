1b3b150257c12a2e20ca7e1d932573cfd76d524c
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-10 14:55:13 +0300

    update: added public server to peers

--- helpers/gun-db.js
+++ helpers/gun-db.js
@@ -2,8 +2,14 @@ import Gun from 'gun/gun'
 import 'gun/sea'
 // import 'gun/lib/path'
 
+const peers = []
+
+if (process.env.NODE_ENV === 'production')
+  peers.push('https://gun-notes.herokuapp.com/gun')
+else peers.push('http://localhost:8765/gun')
+
 export default Gun({
-  peers: ['http://localhost:8765/gun'],
+  peers,
   localStorage: true,
   radisk: false,
 })
